b7cb734a4f785beda6b0e8cef1a38055a4874faf
Daniel Miller <dealingwith@gmail.com>
2011-11-06 21:07:19 -0600

    add model

---
+++ js/model.js
@@ -0,0 +1,31 @@
+
+var App = {};
+
+App.Model = function() {
+
+    // model subscriptions
+
+
+	
+    /*
+    * private vars and functions
+    *
+    */
+
+
+    /*
+    * public vars and functions
+    *
+    */
+
+    var obj = {
+
+        init: function() {
+        },
+
+
+    };
+
+    return obj;
+
+}();
